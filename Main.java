c9daac77434ee226fb666d77d22cfe8c4b2ef532
Ianisop <69714456+Ianisop@users.noreply.github.com>
2020-08-15 11:41:55 +0200

    try system done

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.IOException;
+import java.lang.*;
+
+class Main{
+
+	public static void askPin() throws IOException{
+		int pinCode = 0;
+		int tries = 0;
+		while(pinCode != 0001 && tries < 3){
+			Scanner pin = new Scanner(System.in);
+			System.out.print("Please insert your pincode: ");
+			pinCode = pin.nextInt();
+			System.out.println(System.lineSeparator().repeat(200));
+			tries++;
+			System.out.println(tries);
+			if(pinCode == 0001){
+				System.out.println(System.lineSeparator().repeat(200));
+				System.out.println("Succes!");
+				try{
+					Thread.sleep(2000);
+				}
+				catch(InterruptedException e){
+
+				}
+				System.out.println(System.lineSeparator().repeat(200));
+				System.out.println("Succes!");
+				break;
+			}
+		}
+
+	}
+	public static void main(String[] args) throws IOException{
+		askPin();
+	}
+}
